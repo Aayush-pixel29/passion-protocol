e4b2e8e69f8cee405a86d81c8dd0feacfe2b49c2
Aayush <shelaraayush535@gmail.com>
2026-08-23 14:15:10 +0530

    fix: remove explicit any to satisfy eslint production build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,6 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2026-07-29.dahlia" as any, 
+  apiVersion: "2026-07-29.dahlia", 
   typescript: true,
 });
